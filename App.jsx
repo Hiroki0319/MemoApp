b61e9c7860af8889abc2adb11f1923294fef4430
Hiroki0319 <60523448+Hiroki0319@users.noreply.github.com>
2022-03-16 03:57:58 +0900

    Login Sign screen

--- App.jsx
+++ App.jsx
@@ -2,9 +2,11 @@ import React from 'react';
 
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoListScreen from './src/screens/MemoListScreen';
-import MemoEditScreen from './src/screens/MemoEditScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
 // import MemoCreateScreen from './src/screens/MemoCreateScreen';
+// import LoginScreen from './src/screens/LoginScreen';
+import SignUpScreen from './src/screens/SignUpScreen';
 
 export default function App() {
-  return <MemoEditScreen />;
+  return <SignUpScreen />;
 }
